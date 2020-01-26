9af8558961dd442d235c8487cf588d493e88a356
greg <grzegorz.kozub@gmail.com>
2020-01-26 09:58:31 +0100

    Move some stuff to the bootstrapper

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,2 +1,28 @@
 set -e -o verbose
 
+# zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+
+# tmux
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bindings/install_plugins
+
+systemctl --user enable tmux
+
+# ranger
+
+if [ -d ~/ranger_devicons ]; then rm -rf ~/ranger_devicons; fi
+pushd ~
+git clone https://github.com/alexanderjeurissen/ranger_devicons
+cd ranger_devicons
+make install
+popd
+rm -rf ~/ranger_devicons
+
+# vim and neovim
+
+vim -c 'PlugInstall | exit'
+ln -s ~/.vim  ~/.config/nvim
+
