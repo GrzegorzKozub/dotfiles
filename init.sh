74164b1eaf822b93dd49aba10e18eb2bf31e38bb
greg <grzegorz.kozub@gmail.com>
2020-03-19 20:43:16 +0100

    Extract plugin installation into a separate script

--- init.sh
+++ init.sh
@@ -27,28 +27,3 @@ stow --dir=`dirname $0` --target=$HOME --stow \
 
 ln -sf $(dirname $(realpath $0))/vim/.vim ~/.config/nvim
 
-# zsh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
-# tmux
-
-if [ -d ~/.tmux/plugins/tpm ]; then rm -rf ~/.tmux/plugins/tpm; fi
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux new-session -d
-~/.tmux/plugins/tpm/bindings/install_plugins
-
-# ranger
-
-if [ -d ~/ranger_devicons ]; then rm -rf ~/ranger_devicons; fi
-pushd ~
-git clone https://github.com/alexanderjeurissen/ranger_devicons
-cd ranger_devicons
-make install
-popd
-rm -rf ~/ranger_devicons
-
-# vim and neovim
-
-vim -c 'PlugInstall'
-
