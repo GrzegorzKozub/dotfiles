697d6148eb00df31a4d204db6fec83bfce3ea735
greg <grzegorz.kozub@gmail.com>
2020-03-21 23:35:24 +0100

    Start working on mac compatibility

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 set -e -o verbose
 
+# os
+
+case $(uname -s) in
+  'Linux') LINUX=1;;
+  'Darwin') MAC=1;;
+esac
+
 # repo
 
 pushd `dirname $0`
@@ -13,9 +20,7 @@ popd
 if [ ! -d ~/.config ]; then mkdir ~/.config; fi
 
 stow --dir=`dirname $0` --target=$HOME/.config --stow \
-  keepass \
-  ranger \
-  vscode
+  ranger
 
 stow --dir=`dirname $0` --target=$HOME --stow \
   elixir \
@@ -26,4 +31,24 @@ stow --dir=`dirname $0` --target=$HOME --stow \
   zsh
 
 ln -sf $(dirname $(realpath $0))/vim/.vim ~/.config/nvim
+
+if [[ $MAC ]]; then
+
+  ln -sf $(dirname $(realpath $0))/keepass.mac/KeePassXC \
+    ~/Library/Application\ Support/KeePassXC
+
+  ln -sf $(dirname $(realpath $0))/vscode/Code \
+    ~/Library/Application\ Support/Code
+
+else
+
+  stow --dir=`dirname $0` --target=$HOME/.config --stow \
+    keepass \
+    vscode
+
+fi
+
+# cleanup
+
+unset LINUX MAC
 
