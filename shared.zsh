24d05ae1db0c768d2b09848a5ed5bba0cf3053b2
greg <grzegorz.kozub@gmail.com>
2021-09-06 22:03:17 +0200

    Update go package installation method and only install packages important for vscode extension

--- shared.zsh
+++ shared.zsh
@@ -52,47 +52,20 @@ cargo install \
 
 export GOPATH=${XDG_DATA_HOME:-~/.local/share}/go
 
+go install github.com/go-delve/delve/cmd/dlv@master
+mv $GOPATH/bin/dlv $GOPATH/bin/dlv-dap
+
 for PACKAGE in \
-  github.com/mdempsky/gocode \
   github.com/uudashr/gopkgs/v2/cmd/gopkgs \
   github.com/ramya-rao-a/go-outline \
-  github.com/acroca/go-symbols \
-  golang.org/x/tools/cmd/guru \
-  golang.org/x/tools/cmd/gorename \
-  github.com/cweill/gotests/... \
+  github.com/cweill/gotests/gotests \
   github.com/fatih/gomodifytags \
   github.com/josharian/impl \
-  github.com/davidrjenni/reftools/cmd/fillstruct \
   github.com/haya14busa/goplay/cmd/goplay \
-  github.com/godoctor/godoctor \
   github.com/go-delve/delve/cmd/dlv \
-  github.com/rogpeppe/godef \
-  golang.org/x/tools/cmd/goimports \
-  golang.org/x/lint/golint \
+  honnef.co/go/tools/cmd/staticcheck \
   golang.org/x/tools/gopls
 do
-  TMP="$(mktemp -d)"
-  pushd $TMP
-  echo 'module dotfiles' > $TMP/go.mod
-  go get -v $PACKAGE
-  popd
-  rm -rf $TMP
+  go install -v $PACKAGE@latest
 done
-
-TMP="$(mktemp -d)"
-pushd $TMP
-echo 'module dotfiles' > $TMP/go.mod
-go get -v -d github.com/stamblerre/gocode
-popd
-rm -rf $TMP
-
-TMP="$(mktemp -d)"
-pushd $TMP
-echo 'module dotfiles' > $TMP/go.mod
-go get github.com/stamblerre/gocode
-go build -o $GOPATH/bin/gocode-gomod github.com/stamblerre/gocode
-popd
-rm -rf $TMP
-
-unset TMP
 
