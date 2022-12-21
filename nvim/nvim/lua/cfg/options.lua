f7c66358d89ccdc6af0506ee3af344f28a13eefd
greg <grzegorz.kozub@gmail.com>
2022-12-21 15:39:47 +0100

    Remove options that are already nvim defaults

--- nvim/nvim/lua/cfg/options.lua
+++ nvim/nvim/lua/cfg/options.lua
@@ -1,60 +1,46 @@
-local options = {
-  autoindent = true,
-  autoread = true,
-  backspace = { "indent", "eol", "start" },
-  backup = true,
-  belloff = "all",
-  cursorline = true, -- can break per https://github.com/neovim/neovim/issues/9019
-  encoding = "utf-8",
-  expandtab = true,
-  foldlevelstart = 99,
-  foldmethod = "syntax",
-  hidden = true,
-  history = 1000,
-  hlsearch = true,
-  ignorecase = true,
-  incsearch = true,
-  infercase = true,
-  joinspaces = false,
-  langremap = false,
-  laststatus = 2,
-  lazyredraw = true,
-  listchars = { tab = "→ ", eol = "¬", trail = "·" },
-  mouse = "a",
-  number = true,
-  scrolloff = 3,
-  shiftwidth = 2,
-  showcmd = true,
-  showmatch = true,
-  showmode = false,
-  sidescroll = 1,
-  sidescrolloff = 15,
-  smartcase = true,
-  smartindent = true,
-  smarttab = true,
-  softtabstop = 2,
-  spelllang = { "en_gb", "pl" },
-  splitbelow = true,
-  splitright = true,
-  swapfile = false,
-  tabstop = 2,
-  termguicolors = true,
-  ttimeout = true,
-  ttimeoutlen = 50,
-  undofile = true,
-  wildmenu = true,
-  wildmode = { "longest:full", "full" },
-  wildoptions = "tagfile",
-  wrap = false,
-}
+local M = {}
 
-for k, v in pairs(options) do
-  vim.opt[k] = v
+function M.use()
+  local opts = {
+    backup = true,
+    cursorline = true, -- can break per https://github.com/neovim/neovim/issues/9019
+    expandtab = true,
+    foldlevelstart = 99,
+    foldmethod = "syntax",
+    ignorecase = true,
+    infercase = true,
+    lazyredraw = true,
+    listchars = { tab = "→ ", eol = "¬", trail = "·" },
+    mouse = "a",
+    number = true,
+    scrolloff = 3,
+    shiftwidth = 2,
+    showmatch = true,
+    showmode = false,
+    sidescrolloff = 15,
+    smartcase = true,
+    smartindent = true,
+    softtabstop = 2,
+    spelllang = { "en_gb", "pl" },
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    tabstop = 2,
+    termguicolors = true,
+    undofile = true,
+    wildmode = { "longest:full", "full" },
+    wildoptions = "tagfile",
+    wrap = false,
+  }
+
+  for k, v in pairs(opts) do
+    vim.opt[k] = v
+  end
+
+  vim.opt.diffopt:append({ "algorithm:histogram", "indent-heuristic", "context:3" })
+  vim.opt.fillchars:append({ vert = " " })
+  vim.opt.shortmess:append("I")
+  vim.opt.whichwrap:append("<,>,[,]")
 end
 
-vim.opt.diffopt:append({ "algorithm:histogram", "indent-heuristic", "context:3" })
-vim.opt.display:append("lastline")
-vim.opt.fillchars:append({ vert = " " })
-vim.opt.nrformats:remove("octal")
-vim.opt.shortmess:append("I")
-vim.opt.whichwrap:append("<,>,[,]")
+return M
