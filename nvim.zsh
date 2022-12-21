2a275c03b2c46edb8488a07d81eb48be5443494a
greg <grzegorz.kozub@gmail.com>
2022-12-21 08:10:27 +0100

    Fix the nvim config switcher

--- nvim.zsh
+++ nvim.zsh
@@ -4,11 +4,15 @@ set -e -o verbose
 
 # switch nvim between vim and lua config
 
-if [[ -f ~/.config/nvim/init.lua ]]; then
-  rm ~/.config/nvim
-  ln -s $(dirname $(realpath $0))/vim/vim ~/.config/nvim
+CONFIG=${XDG_CONFIG_HOME:-~/.config}
+
+if [[ -f $CONFIG/nvim/init.lua ]]; then
+  rm $CONFIG/nvim
+  ln -s $(dirname $(realpath $0))/vim/vim $CONFIG/nvim
 else
   rm ~/.config/nvim
-  stow --dir=`dirname $0` --target=~/.config --stow nvim
+  stow --dir=`dirname $0` --target=$CONFIG --stow nvim
 fi
+
+
 
