2e77dee13809bc4df87749420433353a77894cd7
greg <grzegorz.kozub@gmail.com>
2023-01-27 07:39:20 +0100

    re-add golangci-lint

--- shared.zsh
+++ shared.zsh
@@ -50,6 +50,7 @@ cargo install \
 for PACKAGE in \
   github.com/cweill/gotests/gotests \
   github.com/fatih/gomodifytags \
+  github.com/golangci/golangci-lint/cmd/golangci-lint \
   github.com/josharian/impl \
   github.com/haya14busa/goplay/cmd/goplay \
   github.com/go-delve/delve/cmd/dlv \
