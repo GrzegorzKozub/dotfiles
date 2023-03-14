440c87eb1ab92869fa80aa56fd62833e7b3174c2
greg <grzegorz.kozub@gmail.com>
2023-03-14 07:51:19 +0100

    try kde

---
+++ kde.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+set -e -o verbose
+
+# env
+
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
+
+# dirs
+
+[[ -d $XDG_CONFIG_HOME ]] || mkdir -p $XDG_CONFIG_HOME
+
+# links
+
+stow --dir=`dirname $0` --target=$XDG_CONFIG_HOME --stow \
+  plasma
+
