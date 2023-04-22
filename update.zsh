112e90af72c27d0c9632ce87260d41133019dd85
greg <grzegorz.kozub@gmail.com>
2023-04-22 08:37:01 +0200

    prevent zinit update from spamming the terminal

--- update.zsh
+++ update.zsh
@@ -16,7 +16,13 @@ export ZINIT[HOME_DIR]=$XDG_DATA_HOME/zinit
 export ZINIT[ZCOMPDUMP_PATH]=$XDG_CACHE_HOME/zsh/zcompdump
 
 source $ZINIT[HOME_DIR]/bin/zinit.zsh
-zinit update --all --quiet
+
+set +o verbose
+
+zinit self-update
+zinit update --all
+
+set -o verbose
 
 # tmux
 
