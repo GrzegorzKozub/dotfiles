c55577fc0c84c83e0854cb96760405996f8f5cb3
greg <grzegorz.kozub@gmail.com>
2023-07-02 06:33:47 +0200

    lid sucks on hyprland

---
+++ hyprland/hypr/lid.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+if grep 'open' /proc/acpi/button/lid/LID0/state; then
+  hyprctl keyword monitor 'eDP-1, preferred, auto, 2'
+  hyprctl dispatch exec 'pkill hyprpaper; hyprpaper'
+  hyprctl dispatch exec waybar
+else
+  hyprctl keyword monitor 'eDP-1, disable'
+fi
+
