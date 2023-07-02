f876bbdc723a5b17768905c6c4dd89c737a535a1
greg <grzegorz.kozub@gmail.com>
2023-07-02 07:25:53 +0200

    control volume from hyprland

--- hyprland/hypr/lid.sh
+++ hyprland/hypr/lid.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if grep 'open' /proc/acpi/button/lid/LID0/state; then
+if [[ $(grep 'open' /proc/acpi/button/lid/LID0/state) ]]; then
   hyprctl keyword monitor 'eDP-1, preferred, auto, 2'
   hyprctl dispatch exec 'pkill hyprpaper; hyprpaper'
   hyprctl dispatch exec waybar
