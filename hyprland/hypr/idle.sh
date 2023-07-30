896f4e660f71999439bdb091fed8217b6bc52872
greg <grzegorz.kozub@gmail.com>
2023-07-30 11:08:43 +0200

    fix hyprland display off/on

--- hyprland/hypr/idle.sh
+++ hyprland/hypr/idle.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 swayidle -w \
-  timeout 60 'brightnessctl set 0% --save' resume 'brightnessctl --restore' \
-  timeout 300 'pidof swaylock || swaylock' \
+  timeout 60 'brightnessctl set 10% --save' resume 'brightnessctl --restore' \
+  timeout 300 'chayang && hyprctl dispatch dpms off && swaylock' resume 'hyprctl dispatch dpms on' \
   timeout 600 'systemctl suspend' \
   before-sleep 'pidof swaylock || swaylock'
 
