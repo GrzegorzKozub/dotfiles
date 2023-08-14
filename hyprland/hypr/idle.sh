d8be8e8182a66cac600646204669a6742204f10f
greg <grzegorz.kozub@gmail.com>
2023-08-14 08:29:18 +0200

    remove chayang

--- hyprland/hypr/idle.sh
+++ hyprland/hypr/idle.sh
@@ -2,7 +2,7 @@
 
 swayidle -w \
   timeout 60 'brightnessctl set 10% --save' resume 'brightnessctl --restore' \
-  timeout 300 'chayang && hyprctl dispatch dpms off && swaylock' resume 'hyprctl dispatch dpms on' \
+  timeout 300 'hyprctl dispatch dpms off && swaylock' resume 'hyprctl dispatch dpms on' \
   timeout 600 'systemctl suspend' \
   before-sleep 'pidof swaylock || swaylock'
 
