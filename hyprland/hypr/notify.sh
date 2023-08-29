3765a7c57fc7ea7928344e84647958eeba85173d
greg <grzegorz.kozub@gmail.com>
2023-08-29 08:24:30 +0200

    more hyper media bindings

--- hyprland/hypr/notify.sh
+++ hyprland/hypr/notify.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+mute() {
+  local muted=$(~/code/arch/audio.zsh mute)
+  [[ $muted = 'yes' ]] && local icon='󰖁' || local icon='󰕾'
+  dunstify \
+    -h string:x-canonical-private-synchronous:mute \
+    -t 1000 \
+    "$icon"
+}
+
 volume() {
   local vol=$(~/code/arch/audio.zsh $1)
-  if (( $vol < 33 )); then local icon="󰕿"
-  elif (( $vol > 66 )); then local icon="󰕾"
-  else local icon="󰖀"; fi
+  if (( $vol < 33 )); then local icon='󰕿'
+  elif (( $vol > 66 )); then local icon='󰕾'
+  else local icon='󰖀'; fi
   dunstify \
-    -h string:x-canonical-private-synchronous:audio \
+    -h string:x-canonical-private-synchronous:volume \
     -h int:value:$vol \
     -t 1000 \
     "$icon $vol%"
@@ -14,16 +23,17 @@ volume() {
 
 device() {
   local name=$(~/code/arch/audio.zsh $1)
+  [[ $1 = 'sink' ]] && local icon='󰓃' || local icon='󰍬' # else source
   dunstify \
-    -h string:x-canonical-private-synchronous:audio \
+    -h string:x-canonical-private-synchronous:device \
     -t 1000 \
-    "$2 $name"
+    "$icon $name"
 }
 
 # brightness() { string:x-canonical-private-synchronous:brightness }
 
+[[ $1 = 'mute' ]] && mute
 [[ $1 = 'volume' ]] && volume $2
-[[ $1 = 'sink' ]] && device $1 '󰓃'
-[[ $1 = 'source' ]] && device $1 '󰍬'
+[[ $1 = 'device' ]] && device $2
 [[ $1 = 'brightness' ]] && brightness $2
 
