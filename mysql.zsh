9b4c1ba7b13958a11de9e4e82797617dc02bff87
greg <grzegorz.kozub@gmail.com>
2023-12-21 10:03:05 +0100

    cleanup

--- mysql.zsh
+++ mysql.zsh
@@ -6,17 +6,19 @@ set -e -o verbose
 
 pushd `dirname $0`
 
-# git update-index --assume-unchanged mysql/.mysql/workbench/connections.xml
-# git update-index --assume-unchanged mysql/.mysql/workbench/wb_options.xml
-# git update-index --assume-unchanged mysql/.mysql/workbench/wb_state.xml
+# for ITEM ('connections.xml' 'wb_options.xml' 'wb_state.xml')
+#   git update-index --assume-unchanged mysql/.mysql/workbench/$ITEM
 
-git update-index --assume-unchanged dbeaver/DBeaverData/workspace6/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.editors.prefs
-git update-index --assume-unchanged dbeaver/DBeaverData/workspace6/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.workbench.prefs
-git update-index --assume-unchanged dbeaver/DBeaverData/workspace6/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.jkiss.dbeaver.core.prefs
-git update-index --assume-unchanged dbeaver/DBeaverData/workspace6/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.jkiss.dbeaver.ui.app.standalone.prefs
-git update-index --assume-unchanged dbeaver/DBeaverData/workspace6/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.jkiss.dbeaver.ui.statistics.prefs
-git update-index --assume-unchanged dbeaver/DBeaverData/workspace6/.metadata/.plugins/org.jkiss.dbeaver.ui/dialog_settings.xml
-git update-index --assume-unchanged dbeaver/DBeaverData/workspace6/General/.dbeaver/data-sources.json
+for ITEM (
+  '.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.editors.prefs'
+  '.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.workbench.prefs'
+  '.metadata/.plugins/org.eclipse.core.runtime/.settings/org.jkiss.dbeaver.core.prefs'
+  '.metadata/.plugins/org.eclipse.core.runtime/.settings/org.jkiss.dbeaver.ui.app.standalone.prefs'
+  '.metadata/.plugins/org.eclipse.core.runtime/.settings/org.jkiss.dbeaver.ui.statistics.prefs'
+  '.metadata/.plugins/org.jkiss.dbeaver.ui/dialog_settings.xml'
+  'General/.dbeaver/data-sources.json'
+)
+  git update-index --assume-unchanged dbeaver/DBeaverData/workspace6/$ITEM
 
 popd
 
