0a15a79dfff952acd16afcfd4fbf22a6b2c1bc6a
greg <grzegorz.kozub@gmail.com>
2024-01-20 07:32:15 +0100

    fix alacritty font size

--- alacritty/alacritty/font.sh
+++ alacritty/alacritty/font.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-[[ $HOST = 'drifter' || $HOST = 'player' ]] && echo 15
+[[ $HOST = 'drifter' ]] && echo 15
+[[ $HOST = 'player' ]] && echo 12
 [[ $HOST = 'worker' ]] && echo 21
 
