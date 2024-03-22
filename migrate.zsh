56724e4b0906026adb19339ea147d51edb5438fa
greg <grzegorz.kozub@gmail.com>
2024-03-22 14:42:18 +0100

    cleanup migrate

--- migrate.zsh
+++ migrate.zsh
@@ -2,14 +2,4 @@
 
 set -o verbose
 
-# env
-
-if [[ -f ./zsh/zsh/env.zsh ]]; then
-  mv ./zsh/zsh/env.zsh ./zsh/zsh/.env
-  sed -i -e 's/^export //' ./zsh/zsh/.env
-fi
-
-# fetch
-
-rm -rf ~/.cache/zsh
 
