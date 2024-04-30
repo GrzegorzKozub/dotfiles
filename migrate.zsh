c7472583660164c6f402a35c3465e3bc52e4b387
greg <grzegorz.kozub@gmail.com>
2024-04-30 07:03:44 +0200

    reset python & build luasnip

--- migrate.zsh
+++ migrate.zsh
@@ -2,4 +2,15 @@
 
 set -o verbose
 
+# neovim
+
+nvim --headless -c 'Lazy! build LuaSnip' -c 'quitall'
+
+# python
+
+rm -rf ~/.cache/pip
+
+rm -rf ~/.local/bin
+rm -rf ~/.local/include
+rm -rf ~/.local/lib
 
