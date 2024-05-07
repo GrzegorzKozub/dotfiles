99417318a39bf01d0c5a08f3dc6ea548a660e15f
greg <grzegorz.kozub@gmail.com>
2024-05-07 07:42:40 +0200

    migrate cleanup

--- migrate.zsh
+++ migrate.zsh
@@ -2,22 +2,3 @@
 
 set -o verbose
 
-# neovim
-
-git submodule foreach --recursive git pull
-nvim --headless -c 'Lazy! build LuaSnip' -c 'quitall'
-
-# python
-
-rm -rf ~/.cache/pip
-
-rm -rf ~/.local/bin
-rm -rf ~/.local/include
-rm -rf ~/.local/lib
-
-# cargo-update
-
-cargo install \
-  cargo-update \
-  --force --features vendored-libgit2 --features vendored-openssl
-
