0c5f6c4d85e96107d9ae964d1506c66867ed9836
greg <grzegorz.kozub@gmail.com>
2025-03-05 07:56:04 +0100

    plan out links and sessions

---
+++ tmux/tmux/links.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# https://github.com/alberti42/tmux-fzf-links
+
+echo "todo" && read -rn 1 -s
---
+++ tmux/tmux/sessions.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# https://github.com/sainnhe/tmux-fzf
+# https://github.com/cutbypham/tmux-fzf-session-switch
+# https://github.com/joshmedeski/t-smart-tmux-session-manager
+# https://github.com/27medkamal/tmux-session-wizard
+
+echo "todo" && read -rn 1 -s
