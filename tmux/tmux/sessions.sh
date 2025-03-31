92565529beb0c4791fe63c45faf6171a012c166b
greg <grzegorz.kozub@gmail.com>
2025-03-31 08:46:05 +0200

    tmux sessions start

--- tmux/tmux/sessions.sh
+++ tmux/tmux/sessions.sh
@@ -4,5 +4,11 @@
 # https://github.com/cutbypham/tmux-fzf-session-switch
 # https://github.com/joshmedeski/t-smart-tmux-session-manager
 # https://github.com/27medkamal/tmux-session-wizard
+# https://github.com/2KAbhishek/tmux-tea
 
-echo "todo" && read -rn 1 -s
+SESSIONS=$(
+  tmux list-sessions \
+    -F "\e[37m#{session_name}\e[0m #{t/p:session_created} #{session_windows} #{session_attached} \e[36m#{session_path}\e[0m"
+)
+SESSIONS=${SESSIONS//$HOME/\~}
+echo -e "$SESSIONS" | fzf --ansi --height 100% --layout default
