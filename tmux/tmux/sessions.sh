d5de81e70354ffda8fed81270acb2ad61e401394
greg <grzegorz.kozub@gmail.com>
2025-04-01 08:55:24 +0200

    tmux sessions working now

--- tmux/tmux/sessions.sh
+++ tmux/tmux/sessions.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-# https://github.com/sainnhe/tmux-fzf
-# https://github.com/cutbypham/tmux-fzf-session-switch
-# https://github.com/joshmedeski/t-smart-tmux-session-manager
-# https://github.com/27medkamal/tmux-session-wizard
-# https://github.com/2KAbhishek/tmux-tea
+# TODO: icons, current, --header-lines
 
-SESSIONS=$(
+QUERY="
   tmux list-sessions \
-    -F "\e[37m#{session_name}\e[0m #{t/p:session_created} #{session_windows} #{session_attached} \e[36m#{session_path}\e[0m"
+    -F '#{session_name} #{t/p:session_created} #{session_windows} #{session_attached} #{session_path}' | \
+  column --output-separator ' ' --table --table-right 1,3,4 | \
+  sort --key 1 --reverse
+"
+
+KEYS="$(echo -e "\e[35m<Enter>\e[0m \e[37mSwitch\e[0m · \e[35m<C-k>\e[0m \e[37mKill\e[0m · \e[35m<C-n>\e[0m \e[37mNew\e[0m")"
+
+LIST=$(eval "$QUERY") && LIST=${//$HOME/\~}
+
+SELECTED=$(
+  echo -e "$LIST" |
+  fzf --tmux center,33%,33% \
+    --layout default --list-border rounded --input-border rounded \
+    --ansi --color header:bright-black \
+    --header "$KEYS" --header-first \
+    --bind "ctrl-k:execute-silent(tmux kill-session -t {1})+reload($QUERY)" \
+    --bind "ctrl-n:execute-silent(tmux new-session -d)+reload($QUERY)" \
+    --accept-nth 1
 )
-SESSIONS=${SESSIONS//$HOME/\~}
-echo -e "$SESSIONS" | fzf --ansi --height 100% --layout default
+
+EXIT=$? && [ $EXIT == 0 ] && tmux switch-client -t "$SELECTED" || exit 0
+
